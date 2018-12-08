1b826d887d9780f78a0b5b2c9af81d3c6fde2f01
Leon Jongsma <l.jongsma@tfe.nl>
2018-12-08 19:16:11 +0100

    project setup

---
+++ site/src/main/java/nl/tfe/hipporesearch/beans/BaseDocument.java
@@ -0,0 +1,9 @@
+package nl.tfe.hipporesearch.beans;
+
+import org.hippoecm.hst.content.beans.Node;
+import org.hippoecm.hst.content.beans.standard.HippoDocument;
+
+@Node(jcrType="hipporesearch:basedocument")
+public class BaseDocument extends HippoDocument {
+
+}
